bd9e433a6b5b602cb5cbe1a0f9c1927eba3a384b
erxide <erwan.sinck12@gmail.com>
2024-11-13 12:17:02 +0100

    update

--- src/Routes/Healthcheck.ts
+++ src/Routes/Healthcheck.ts
@@ -1,9 +1,16 @@
 import { Router, Request, Response } from 'express';
+import { client } from '../connexiondb'
 
 const router = Router();
 
 router.get('/', async (req: Request, res: Response)=> {
-    res.status(200).send("ok\n")
-})
+    try {
+        await client.db("admin").command({ping:1});
+        res.status(200).send("OK\n")
+    } catch (error) {
+        console.error('MongoDB est déconnecté :', error);
+        res.status(500).send("KO\n")
+    }
+});
 
 export default router;
