0b48bc123e8b1a619053491dbc91807df20e42dd
aghugo <hugo.aguer@ynov.com>
2024-11-13 11:33:49 +0100

    connectotodb

---
+++ src/connexiondb.ts
@@ -0,0 +1,24 @@
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
+
+dotenv.config();
+const uri : string = process.env.DB_CONN_string|| '';
+
+const client = new MongoClient(uri);
+const connectToDatabase = async () => {
+    try {
+        await client.connect();
+        console.log('Connexion réussie à MongoDB');
+    } catch (err) {
+        console.error('Erreur de connexion à MongoDB :', err);
+        process.exit(1)
+    }
+
+
+
+
+
+
+
+}
+export { connectToDatabase, client };
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import dotenv from 'dotenv';
 import express, {Request, Response} from 'express';
 import HealthcheckRoutes from './Routes/Healthcheck'
+import { connectToDatabase } from './connexiondb';
 
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use('/healthcheck', HealthcheckRoutes)
+app.use('/healthcheck', HealthcheckRoutes);
 const API_PORT=process.env.API_PORT;
 const main = async () =>{
+    connectToDatabase();
     app.listen(API_PORT, () => {
         console.log(`server running on http://localhost:${API_PORT}`);
     }
