67a3368d8cee5075c34e7d1c464bb161210a102d
Matheo-Msst <mmaussant08@gmail.com>
2024-11-13 13:37:29 +0100

    ajout interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,26 @@
+interface Client {
+    phone : string,
+    mail : string,
+    id_client : string,
+    passcode : string,
+    birthdate : string,
+    address : string,
+    zipcode : number,
+    country : string,
+    amount : number,
+}
+interface Compte {
+    id_client : number,
+    accountnumber : number,
+    amount : number,
+    cardnumber : number,
+    ccv : number,
+    carddate : string,
+}
+interface Virement {
+    id : number,
+    senderaccount : number ,
+    recieveraccount : number ,
+    amount : number ,
+    transferdate : Date,
+}
